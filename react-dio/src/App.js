9a1d1a56be78ecca033c0ffe8467686563c836fd
matusilva <matheus10xy@gmail.com>
2021-11-09 22:20:39 -0300

    criando meu primeiro component

--- react-dio/src/App.js
+++ react-dio/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>My first React app</h1>
+      <ul>
+        <li class="list-group-item list-group-item-dark">Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </>
+  )
 }
 
 export default App;
---
+++ react-dio/src/components/Item/index.js
@@ -0,0 +1,7 @@
+const Item = () => {
+    return (
+        <li class="list-group-item list-group-item-dark">Item 1</li>
+    )
+}
+
+export default Item;
