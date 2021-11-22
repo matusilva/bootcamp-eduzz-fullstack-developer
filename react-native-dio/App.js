fcf6321a38f0133b8ae05a591cf64b574afb69f8
matusilva <matheus10xy@gmail.com>
2021-11-21 22:54:53 -0300

    criando gerador de numeros aleatorios

--- react-native-dio/App.js
+++ react-native-dio/App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TouchableOpacity
+} from 'react-native'
 
-export default function App() {
+const App = () => {
+
+  const [numero, setNumero] = useState(0)
+
+  function handleNumero() {
+    const novo_numero = Math.floor(Math.random() * 100)
+    setNumero(novo_numero)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>My first React-Native App !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <SafeAreaView style={style.container}>
+        <Text style={style.number}>{numero}</Text>
+        <TouchableOpacity onPress={handleNumero} style={style.button}>
+          <Text>Gerar número</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
+    backgroundColor: '#FF0000',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  number: {
+    fontSize: 38,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#fff',
+    width: '50%',
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 5
+  }
+})
+
+export default App
