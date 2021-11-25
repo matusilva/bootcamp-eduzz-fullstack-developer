a0f3eca0825fbf4304b34e82e70567ced3907f24
matusilva <matheus10xy@gmail.com>
2021-11-25 00:11:12 -0300

    concluindo primeira aula: construindo pasta dos components e service api

--- github-api/src/App.js
+++ github-api/src/App.js
@@ -1,8 +1,32 @@
+import React from "react";
+import Layout from "./components/layout";
+
 function App() {
   return (
-    <div className="App">
-      Github Api
-    </div>
+    <main>
+      <Layout>
+        <div>
+          <img source="https://avatars.githubusercontent.com/u/25967644?v=4" alt="avatar of user" />
+          <h1>Matheus Silva</h1>
+          <h3>Username: </h3>
+          <span>matusilva</span>
+          <div>
+            <div>
+              <h4>Followers</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Stareds</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Followings</h4>
+              <span>5</span>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </main>
   );
 }
 
