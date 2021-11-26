571567650e296f6d2b100e0ae3eee2b64f55bab3
matusilva <matheus10xy@gmail.com>
2021-11-25 23:40:06 -0300

    melhorando codigo

--- server-node-dio/server.js
+++ server-node-dio/server.js
@@ -1,5 +1,7 @@
 const http = require('http');
+const host = 'http://localhost';
+const port = 3000;
 
 http.createServer((req, res) => {
     res.end('<h1>Funcionando</h1>')
-}).listen(3000, () => console.log('server is running'));
+}).listen(port, () => console.log(`server is running in ${host}:${port}`));
