0c0a348d244f5ce736ef8caf7e01604b29c14fa3
matusilva <matheus10xy@gmail.com>
2021-11-25 23:58:06 -0300

    implementando a função setInterval

--- server-node-dio/pcRamUsage.js
+++ server-node-dio/pcRamUsage.js
@@ -1,14 +1,21 @@
 const os = require('os');
-const { arch, platform, totalmem, freemem } = os;
 
-const tRam = totalmem() / 1024 / 1024;
-const fRam = freemem() / 1024 / 1024;
 
-const stats = {
-    OS: platform(),
-    Arch: arch(),
-    TotalRAM: parseInt(tRam),
-    FreeRAM: parseInt(fRam),
-}
+setInterval(() => {
+    const { arch, platform, totalmem, freemem } = os;
+    const tRam = totalmem() / 1024 / 1024;
+    const fRam = freemem() / 1024 / 1024;
+    const usage = (fRam / tRam) * 100;
 
-console.log(stats);
+    const stats = {
+        OS: platform(),
+        Arch: arch(),
+        TotalRAM: `${parseInt(tRam)} MB`,
+        FreeRAM: `${parseInt(fRam)} MB`,
+        Usage: `${usage.toFixed(2)}%`
+    }
+
+    console.clear();
+    console.table(stats);
+}, 1000);
+
