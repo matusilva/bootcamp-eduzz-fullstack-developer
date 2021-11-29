970596f349e171a6955612ea21d0cfb5387e02c2
matusilva <matheus10xy@gmail.com>
2021-11-28 22:50:16 -0300

    automatizando o servidor para identificar alterações no codigo

--- node-api-dio/src/index.ts
+++ node-api-dio/src/index.ts
@@ -3,9 +3,9 @@ import express, {Request, Response, NextFunction} from 'express';
 const app = express(); // instanciar a aplicação
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({foo: 'bar'});
+    res.status(200).send({foo: 'Testando ts-node-dev'});
 });
 
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
-})
+});
