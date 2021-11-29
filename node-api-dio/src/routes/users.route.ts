d1c2e5d4253b5660e1cfe545a03252f996724c8b
matusilva <matheus10xy@gmail.com>
2021-11-28 23:26:51 -0300

    implementando rota por id

--- node-api-dio/src/routes/users.route.ts
+++ node-api-dio/src/routes/users.route.ts
@@ -1,10 +1,17 @@
 import { Request, Response, NextFunction, Router } from 'express';
+import {StatusCodes} from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{userName: 'Matheus'}];
-    res.status(200).json(users);
+    res.status(StatusCodes.OK).json(users);
 });
+
+usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({uuid});
+});
+
 
 export default usersRoute;
