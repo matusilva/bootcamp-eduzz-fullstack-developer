f949ed45c62aed8f6ad527a734e66946487e8767
matusilva <matheus10xy@gmail.com>
2021-11-28 22:41:12 -0300

    criando servidor com express tipado

--- node-api-dio/src/index.ts
+++ node-api-dio/src/index.ts
@@ -1,1 +1,11 @@
-console.log('executei');
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express(); // instanciar a aplicação
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'bar'});
+});
+
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!');
+})
